bf6cb49d71dd9991b496d6ceb810e663ce9f5ae1
A-Rush-R <arush.up@gmail.com>
2025-01-31 20:50:17 +0530

    implement auth and low level functions

--- http_server.cpp
+++ http_server.cpp
@@ -32,23 +32,195 @@ class HTTP_Server : public TCP_Server
 
     static const int BUFFER_SIZE = 1024;
 
-    bool authenticate_user(int client_socket); // to be filled
+    bool authenticate_user(int client_socket){
+        char buffer[BUFFER_SIZE];
+        memset(buffer, 0, BUFFER_SIZE);
 
+        string username_prompt = "Enter username: ";
+        send(client_socket, username_prompt.c_str(), username_prompt.length(), 0);
+        recv(client_socket, buffer, BUFFER_SIZE, 0);
+        string username(buffer);
+
+        memset(buffer, 0, BUFFER_SIZE);
+        string password_prompt = "Enter password: ";
+        send(client_socket, password_prompt.c_str(), password_prompt.length(), 0);
+        recv(client_socket, buffer, BUFFER_SIZE, 0);
+        string password(buffer);
+
+        users_lock.lock();
+        if (users.find(username) == users.end() || users[username] != password)
+        {
+            string auth_failed = "Authentication failed.";
+            send(client_socket, auth_failed.c_str(), auth_failed.length(), 0);
+            users_lock.unlock();
+            close(client_socket);
+            return false;
+        }
+        users_lock.unlock();
+
+        string welcome_message = "Welcome to the chat server !";
+        send(client_socket, welcome_message.c_str(), welcome_message.length(), 0);
+
+        clients_lock.lock();
+        clients[client_socket] = username;
+        unordered_set<int> recipients;
+        for (const auto &[sock, user] : clients)
+        {
+            if (sock != client_socket)
+            {
+                recipients.insert(sock);
+            }
+        }
+        clients_lock.unlock();
+
+        sockets_lock.lock();
+        sockets[username] = client_socket;
+        sockets_lock.unlock();
+
+        string join_message = username + " has joined the chat.";
+        send_message(recipients, join_message);
+
+        thread client_thread(client_handler, client_socket);
+        client_thread.detach();
+        return true;
+    }
 
     // handlers for commands, to be shifted
-    void handle_broadcast(int client_socket, const string& message);
-    void handle_private_message(int client_socket, string& request);
-    void handle_create_group(int client_socket, const string& group_name);
-    void handle_join_group(int client_socket, const string& group_name);
-    void handle_leave_group(int client_socket, const string& group_name);
-    void handle_group_message(int client_socket, const string& request);
-    void handle_client_exit(int client_socket);
+    void handle_broadcast(int client_socket, const string& message){
+        // string client = clients[client_socket];
+
+        // string response = "[Broadcast from " + client + "]: " + message;
+    }
+
+    void handle_private_message(int client_socket, string& request){
+        
+    }
+
+    void handle_create_group(int client_socket, const string& group_name){
+
+    }
+
+    void handle_join_group(int client_socket, const string& group_name){
+
+    }
+
+    void handle_leave_group(int client_socket, const string& group_name){
+
+    }
+
+    void handle_group_message(int client_socket, const string& request){
+
+    }
+
+    void handle_client_exit(int client_socket){
+        clients_lock.lock();
+        string username = clients[client_socket];
+        clients.erase(client_socket);
+        clients_lock.unlock();
+
+        sockets_lock.lock();
+        sockets.erase(username);
+        sockets_lock.unlock();
+
+        groups_lock.lock();
+        for (auto &[group_name, members] : groups)
+        {
+            lock_guard<mutex> group_lock(group_locks[group_name]);
+            members.erase(client_socket);
+        }
+        groups_lock.unlock();
+
+        close(client_socket);
+        cout << "Client " << username << " disconnected." << endl;
+    }
 
     // lowest level network commands
-    STATUS send_message(const unordered_set<int> &recv_sockets, const string message);
-    STATUS create_group(const string group_name);
-    STATUS join_group(const string group_name, const int socket);
-    STATUS leave_group(const string group_name, const int socket);
+    STATUS send_message(const unordered_set<int> &recv_sockets, const string message){    for (auto &sock : recv_sockets)
+        {
+            client_locks[sock].lock();
+            send(sock, message.c_str(), message.length(), 0);
+            client_locks[sock].unlock();
+        }
+
+        return SUCCESS;
+    }
+    STATUS create_group(const string group_name){
+        groups_lock.lock();
+
+        if (groups.find(group_name) != groups.end())
+        {
+            cout << "Group " << group_name << " already exists." << endl;
+            groups_lock.unlock();
+
+            return GROUP_EXISTS;
+        }
+
+        groups[group_name] = {};
+        group_locks[group_name];
+
+        cout << "Group " << group_name << " created." << endl;
+
+        groups_lock.unlock();
+
+        return SUCCESS;
+    }
+
+    STATUS join_group(const string group_name, const int socket){
+        groups_lock.lock();
+
+        if (groups.find(group_name) == groups.end())
+        {
+            cout << "Group " << group_name << " does not exist." << endl;
+            groups_lock.unlock();
+            return INVALID_GROUP_NAME;
+        }
+
+        group_locks[group_name].lock();
+
+        if (groups[group_name].find(socket) != groups[group_name].end())
+        {
+            cout << "Client " << clients[socket] << " already in group " << group_name << endl;
+            group_locks[group_name].unlock();
+            groups_lock.unlock();
+            return USER_ALREADY_IN_GROUP;
+        }
+
+        groups[group_name].insert(socket);
+        cout << "Client " << clients[socket] << " joined group " << group_name << endl;
+        group_locks[group_name].unlock();
+
+        groups_lock.unlock();
+
+        return SUCCESS;
+    }
+
+    STATUS leave_group(const string group_name, const int socket){
+        groups_lock.lock();
+
+        if (groups.find(group_name) == groups.end())
+        {
+            cout << "Group " << group_name << " does not exist." << endl;
+            groups_lock.unlock();
+            return INVALID_GROUP_NAME;
+        }
+
+        group_locks[group_name].lock();
+
+        if (groups[group_name].find(socket) == groups[group_name].end())
+        {
+            cout << "Client " << clients[socket] << " not in group " << group_name << endl;
+            group_locks[group_name].unlock();
+            groups_lock.unlock();
+            return USER_NOT_IN_GROUP;
+        }
+
+        groups[group_name].erase(socket);
+        cout << "Client " << clients[socket] << " left group " << group_name << endl;
+        group_locks[group_name].unlock();
+
+        groups_lock.unlock();
+        return SUCCESS;
+    }
     
 
 
@@ -67,10 +239,10 @@ class HTTP_Server : public TCP_Server
         users = initialise_users();
         create_and_bind_socket(PORT);
     }
-    void start_listening(int num_users)
-    {
-        start_listening(num_users);
-    }
+    // void start_listening(int num_users)
+    // {
+    //     start_listening(num_users);
+    // } already defined in TCP_Server
     void continuous_accept()
     {
         struct sockaddr_in address;
