e3800b1bf944f1926bc141c70eda135aa9a71f68
A-Rush-R <arush.up@gmail.com>
2025-02-04 15:15:36 +0530

    missed return in private msg

--- http_server.cpp
+++ http_server.cpp
@@ -175,6 +175,8 @@ class HTTP_Server : public TCP_Server
 			client_locks[client_socket].lock();
             send(client_socket, response.c_str(), response.length(), 0);
 			client_locks[client_socket].unlock();
+
+            return;
 		}
 
         string client = clients[client_socket];
